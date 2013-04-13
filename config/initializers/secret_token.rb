8b549acebb3615ca05f7d4c783bbab9134c68dc4
admin <admin@www11385ue.sakura.ne.jp>
2013-04-13 15:34:00 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ordersheet::Application.config.secret_key_base = 'c9434d879d45ad2286377097f35a0ab5f6bfa8c458dc4e01ef01f4a73597fdc62f7efa0d656c7fa7f73a01492e3364dc789ef64dd6ddc2f30525e5171ecb6ff7'
