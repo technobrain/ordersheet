d9af05b891b9c684f2dbea94b4bcb5d12041d48b
takahashi-tbn <kotaro_takahashi@tbn.co.jp>
2013-04-26 14:04:48 +0900

    second commit

--- app/models/order_info.rb
+++ app/models/order_info.rb
@@ -3,7 +3,7 @@ class OrderInfo < ActiveRecord::Base
 #  has_one  :unit_masters   :foreign_key => "unit_cd"
 #  has_one  :unit_masters   :foreign_key => "revenue_unit_code"
   has_one  :project_masters, :foreign_key => "project_cd"
-  has_one  :buisiness_masters   :foreign_key => "business_cd"
-  has_one  :payment_site_masters :foreign_key => "payment_site"
+  has_one  :buisiness_masters,   :foreign_key => "business_cd"
+  has_one  :payment_site_masters, :foreign_key => "payment_site"
   has_many :order_charge_people, :foreign_key =>  "order_no"
 end
