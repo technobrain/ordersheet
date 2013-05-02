dcc10df7256fcca033f0aad81ae16e74398f2199
kotaro takahashi <kotaro_takahashi@tbn.co.jp>
2013-05-02 23:34:32 +0900

    update

--- app/controllers/order_infos_controller.rb
+++ app/controllers/order_infos_controller.rb
@@ -69,7 +69,7 @@ class OrderInfosController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def order_info_params
-    params.require(:order_info).permit(:order_no, :company_cd, :unit_cd, :author, :revenue_unit_code, :project_cd, :business_cd, :contract_start_date, :contract_nd_date, :work_place, :income_date, :payment_site, :work_start_date, :work_end_date, :other, :general_receipt_date, :general_order_date, :general_order_no, :geneal_other)
+    params.require(:order_info).permit(:order_no, :company_cd, :unit_cd, :author, :revenue_unit_code, :project_cd, :business_cd, :contract_start_date, :contract_nd_date, :work_place, :income_date, :payment_site, :work_start_date, :work_end_date, :other, :general_receipt_date, :general_order_date, :general_order_no, :general_other)
   end
 
   def set_order_masters
