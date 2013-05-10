c44d41cda2e73a21cc7d7534d55b9c266c286cdd
kotaro takahashi <kotaro_takahashi@tbn.co.jp>
2013-05-11 02:56:27 +0900

    update

--- app/controllers/order_infos_controller.rb
+++ app/controllers/order_infos_controller.rb
@@ -74,7 +74,7 @@ class OrderInfosController < ApplicationController
 
   def set_order_masters
     @order_infos = OrderInfo.new
-    @order_charge_people = OrderChargePeople.new
+    @order_charge_people = OrderChargePerson.new
     @project_masters = ProjectMaster.all
     @business_masters = BusinessMaster.all
     @company_masters = CompanyMaster.all
