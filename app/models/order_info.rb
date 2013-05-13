82c02e8778e46876415f0cf85e59ef90240ee14d
kotaro takahashi <kotaro_takahashi@tbn.co.jp>
2013-05-13 23:11:44 +0900

    commit from desktop

--- app/models/order_info.rb
+++ app/models/order_info.rb
@@ -1,7 +1,7 @@
 class OrderInfo < ActiveRecord::Base
-   has_many :order_charge_person, :foreign_key =>  "order_id", :class_name => "OrderChargePerson"
+   has_many :order_charge_people, :foreign_key =>  "order_id"
    has_many :order_break_time, :foreign_key =>  "order_id"
    has_many :order_date, :foreign_key =>  "order_id"
-   accepts_nested_attributes_for :order_charge_person ,allow_destroy: true
+   accepts_nested_attributes_for :order_charge_people ,allow_destroy: true
 
 end
