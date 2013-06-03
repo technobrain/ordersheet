603a5cf85a6707f462426620ba8ccbcd454bae82
takahashi-tbn <kotaro_takahashi@tbn.co.jp>
2013-06-04 01:44:56 +0900

    upload add

--- app/controllers/order_infos_controller.rb
+++ app/controllers/order_infos_controller.rb
@@ -16,6 +16,7 @@ class OrderInfosController < ApplicationController
   def new
     @order_infos = OrderInfo.new
   #  9.times { @order_infos.order_charge_people.build }
+    @order_infos.order_charge_people.build
     3.times { @order_infos.order_break_times.build }
     3.times { @order_infos.order_dates.build }
     set_order_masters
