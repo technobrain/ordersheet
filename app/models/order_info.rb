6ae61f2d291c75b96d2b7432bf29124e233abfab
takahashi-tbn <kotaro_takahashi@tbn.co.jp>
2013-06-09 12:12:22 +0900

    commit from AsUS

--- app/models/order_info.rb
+++ app/models/order_info.rb
@@ -2,8 +2,8 @@ class OrderInfo < ActiveRecord::Base
    has_many :order_charge_people, :foreign_key =>  "order_id"
    has_many :order_break_times, :foreign_key =>  "order_id"
    has_many :order_dates, :foreign_key =>  "order_id"
-   accepts_nested_attributes_for :order_charge_people ,allow_destroy: true,
-       :reject_if => lambda{ |attrs| attrs[:order_id].blank? }
+   accepts_nested_attributes_for :order_charge_people ,allow_destroy: true
+  #     :reject_if => lambda{ |attrs| attrs[:order_id].blank? }
    accepts_nested_attributes_for :order_break_times ,allow_destroy: true
    accepts_nested_attributes_for :order_dates ,allow_destroy: true
 
