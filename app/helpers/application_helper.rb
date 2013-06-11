2e4c6393c5359b12f2cdc0c0fe347082d40c4dbc
takahashi-tbn <kotaro_takahashi@tbn.co.jp>
2013-06-12 00:38:09 +0900

    upload add

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,6 @@ module ApplicationHelper
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
       render(association.to_s.singularize + "_fields", f: builder)
     end
-    button_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")}, :style=>"color:blue;")
   end
 end
