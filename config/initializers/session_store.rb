ad43cd02f8f77b013ef1000c736505950333da67
takahashi-tbn <kotaro_takahashi@tbn.co.jp>
2013-06-14 00:52:54 +0900

    rails update to rc2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Ordersheet::Application.config.session_store :encrypted_cookie_store, key: '_ordersheet_session'
+Ordersheet::Application.config.session_store :cookie_store, key: '_ordersheet_session'
