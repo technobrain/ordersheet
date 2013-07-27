eb6236b9a6dcff91f7a0660f9a96445c5d65ba1c
kotaro takahashi <kotaro_takahashi@tbn.co.jp>
2013-07-27 23:01:12 +0900

    commit from desktop

--- app/models/order_info.rb
+++ app/models/order_info.rb
@@ -8,5 +8,11 @@ class OrderInfo < ActiveRecord::Base
    accepts_nested_attributes_for :order_break_times ,allow_destroy: true
    accepts_nested_attributes_for :order_dates ,allow_destroy: true
 
+   HUMANIZED_KEY_NAMES = {"company_id" => "企業名",
+                          "unit_id" => "所属",
+                          "revenue_unit_id" => "売上げ計上事業部",
+                          "project_id" => "案件名",
+                          "business_id" => "業務名"}
+
    validates_presence_of :company_id, :unit_id, :revenue_unit_id, :project_id, :business_id, :contract_start_date, :contract_end_date, :income_date, :payment_site_id, :work_start_date, :work_end_date, :message => 'が入力されていません。'
 end
